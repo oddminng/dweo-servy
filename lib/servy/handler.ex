cfaf767df520a05a5c03f82396bf436e87e23909
oddminng <oddminng@gmail.com>
2020-01-26 16:21:33 +0800

    pragstudio-elixir-11-organizing-code

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,8 +1,10 @@
 defmodule Servy.Handler do
-
   @moduledoc "Handles HTTP requests."
 
   @pages_path Path.expand("../../pages", __DIR__)
+
+  import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
+  import Servy.Parser, only: [parse: 1]
 
   @doc "Transforms the request into a response."
   def handle(request) do
@@ -14,36 +16,6 @@ defmodule Servy.Handler do
     |> track
     |> format_response
   end
-
-  @doc "Logs 404 requests"
-  def track(%{status: 404, path: path} = conv) do
-    IO.puts("Warning: #{path} is on the loose!")
-    conv
-  end
-
-  def track(conv), do: conv
-
-  def rewrite_path(%{path: "wildlife"} = conv) do
-    %{conv | path: "/wildthings"}
-  end
-
-  def rewrite_path(conv), do: conv
-
-  def log(conv), do: IO.inspect(conv)
-
-  def parse(request) do
-    [method, path, _] =
-      request
-      |> String.split("\n")
-      |> List.first()
-      |> String.split(" ")
-
-    %{method: method, path: path, resp_body: "", status: nil}
-  end
-
-  # def route(conv) do
-  #   route(conv, conv.method, conv.path)
-  # end
 
   def route(%{method: "GET", path: "/wildthings"} = conv) do
     %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
@@ -62,6 +34,10 @@ defmodule Servy.Handler do
     |> Path.join("about.html")
     |> File.read()
     |> handle_file(conv)
+  end
+
+  def route(%{path: path} = conv) do
+    %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
   def handle_file({:ok, content}, conv) do
@@ -92,10 +68,6 @@ defmodule Servy.Handler do
   #       %{conv | status: 500, resp_body: "File error: #{reason}"}
   #   end
   # end
-
-  def route(%{path: path} = conv) do
-    %{conv | status: 404, resp_body: "No #{path} here!"}
-  end
 
   def format_response(conv) do
     """
